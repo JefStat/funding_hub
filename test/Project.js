5818e06ff5c3d0a2f4d81d4bfc730c2cc6f2755e
Jef _ <jef.statham@gmail.com>
2016-08-25 01:37:14 -0400

    passing refund test

--- test/Project.js
+++ test/Project.js
@@ -1,7 +1,6 @@
-
 // ref https://github.com/tcoulter/dao-truffle/blob/master/index.js
 function jump(duration) {
-    return function(callback) {
+    return function (callback) {
         console.log("Jumping " + duration + "s ...");
         var seconds = Math.floor(new Date().getTime() / 1000) + duration;
 
@@ -29,9 +28,9 @@ var ProjectDetailsStruct = {
 
 var assertDetails = function (expected, d) {
     assert.equal(expected.owner, d.owner, 'owner does not equal');
-    assert.equal(expected.goalAmount, d.goalAmount, 'goal amount does not equal');
-    assert.equal(expected.deadline, d.deadline, 'deadline does not equal');
-    assert.equal(expected.amountFunded, d.amountFunded, 'amount funded does not equal');
+    assert.equal('' + expected.goalAmount, d.goalAmount.toString(), 'goal amount does not equal');
+    assert.equal('' + expected.deadline, d.deadline.toString(), 'deadline does not equal');
+    assert.equal('' + expected.amountFunded, d.amountFunded.toString(), 'amount funded does not equal');
     assert.equal(expected.refunded, d.refunded, 'refunded does not equal');
     assert.equal(expected.paid, d.paid, 'paid does not equal');
 };
@@ -41,7 +40,7 @@ contract('Project', function (accounts) {
         var expected = {
             owner: accounts[0],
             goalAmount: 100000,
-            deadline: Math.floor(Date.now() / 1000) + 60,
+            deadline: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
             amountFunded: 0,
             refunded: false,
             paid: false
@@ -62,7 +61,7 @@ contract('Project', function (accounts) {
         var expected = {
             owner: accounts[0],
             goalAmount: 100000,
-            deadline: Math.floor(Date.now() / 1000) + 60,
+            deadline: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
             amountFunded: 10,
             refunded: false,
             paid: false
@@ -89,7 +88,7 @@ contract('Project', function (accounts) {
         var expected = {
             owner: accounts[0],
             goalAmount: 10,
-            deadline: Math.floor(Date.now() / 1000) + 60,
+            deadline: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
             amountFunded: 10,
             refunded: false,
             paid: true
@@ -100,7 +99,7 @@ contract('Project', function (accounts) {
                 return p.fund({from: accounts[0], value: 10})
                     .then(function (tx) {
                         console.log('[TEST][Project]  fund 1 tx: ', tx);
-                        return  p.fund({from: accounts[0], value: 10});
+                        return p.fund({from: accounts[0], value: 10});
                     })
                     .then(function (tx) {
                         console.log('[TEST][Project]  fund 2 tx: ', tx);
@@ -137,38 +136,78 @@ contract('Project', function (accounts) {
                     });
             });
     });
-    it.only('should refund when goal failed', function () {
-        var expected = {
-            owner: accounts[0],
-            goalAmount: 10,
-            deadline: Math.floor(Date.now() / 1000) + 60,
-            amountFunded: 1,
-            refunded: true,
-            paid: false
-        };
-        return Project.new(expected.owner, expected.goalAmount, expected.deadline)
-            .then(function (p) {
-                console.log('[TEST][Project]  new project: ', p.address);
-                return p.fund({from: accounts[0], value: 1})
-                    .then(function (tx) {
-                        console.log('[TEST][Project]  fund 1 tx: ', tx);
-                        return new Promise(function (resolve, reject) {
-                            jump(62)(resolve);
+
+    it('should refund when goal failed', function () {
+        var snapshotId = 12345;
+        // since this test messes with testrpc's time it cannot be ran back to back or after any other tests unless
+        // snapshot and revert are used to undo the time jump
+        web3.currentProvider.sendAsync({
+            jsonrpc: "2.0",
+            method: "evm_snapshot",
+            id: snapshotId
+        }, function (err, result) {
+            // this is your callback
+            console.log('[TEST][Project]  evm_snapshot: ', snapshotId);
+            var expected = {
+                owner: accounts[0],
+                goalAmount: 10,
+                deadline: Math.floor(Date.now() / 1000) + 60,
+                amountFunded: 1,
+                refunded: true,
+                paid: false
+            };
+
+            var ownerStartingBalance = web3.eth.getBalance(accounts[0]);
+            var funder1Balance = web3.eth.getBalance(accounts[1]);
+            var acc2Balance = web3.eth.getBalance(accounts[2]);
+
+            return Project.new(expected.owner, expected.goalAmount, expected.deadline, {from: accounts[2]})
+                .then(function (p) {
+                    console.log('[TEST][Project]  new project: ', p.address);
+                    return p.fund({from: accounts[1], value: 1})
+                        .then(function (tx) {
+                            console.log('[TEST][Project]  fund 1 tx: ', tx);
+                            funder1Balance = web3.eth.getBalance(accounts[1]); //balance after gas spent to fund
+                            return p.details.call();
+                        })
+                        .then(function (details) {
+                            var d = ProjectDetailsStruct.new(details);
+                            console.log('[TEST][Project]  details after fund 1', d);
+                            return new Promise(function (resolve, reject) {
+                                jump(62)(resolve);
+                            });
+                        })
+                        .then(function () {
+                            console.log('[TEST][Project]  jump');
+                            return p.fund({from: accounts[2], value: 1});
+                        })
+                        .then(function (tx) {
+                            console.log('[TEST][Project]  fund late: ', tx);
+                            return p.details.call();
+                        })
+                        .then(function (details) {
+                            var d = ProjectDetailsStruct.new(details);
+                            console.log('[TEST][Project]  details ', d);
+                            assertDetails(expected, d);
+
+                            assert.equal(ownerStartingBalance.toString(), web3.eth.getBalance(accounts[0]).toString(), 'owner balance changed');
+                            assert.equal(funder1Balance.plus(1).toString(), web3.eth.getBalance(accounts[1]).toString(), 'funder 1 balance changed');
+                            return new Promise(function (resolve, reject) {
+                                web3.currentProvider.sendAsync({
+                                    jsonrpc: "2.0",
+                                    method: "evm_revert",
+                                    id: snapshotId
+                                }, function (err, result) {
+                                    if (err) {
+                                        reject(err);
+                                        return;
+                                    }
+                                    console.log('[TEST][Project]  evm_revert: ', snapshotId);
+                                    resolve(result);
+                                });
+                            });
                         });
-                    })
-                    .then(function (tx) {
-                        console.log('[TEST][Project]  jump');
-                        return p.fund({from: accounts[0], value: 1});
-                    })
-                    .then(function (tx) {
-                        console.log('[TEST][Project]  fund late: ', tx);
-                        return p.details.call();
-                    })
-                    .then(function (details) {
-                        var d = ProjectDetailsStruct.new(details);
-                        console.log('[TEST][Project]  details ', d);
-                        assertDetails(expected, d);
-                    });
-            });
+                });
+        });
     });
 });
