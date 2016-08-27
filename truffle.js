f68db8771a9a1e3b5efafc3dda88c7fd7444930d
Jef _ <jef.statham@gmail.com>
2016-08-26 21:28:22 -0400

    Proper staging addresses

--- truffle.js
+++ truffle.js
@@ -22,17 +22,18 @@ module.exports = {
     port: 8546
   }
   ,networks: {
-        "live": {
-            network_id: 1 // Ethereum public network
-            // ,host: "192.168.1.16"
-            // ,port: 8545
-        }
-        , "morden": {
-            network_id: 2       // Official Ethereum test network
-            // ,host: "192.168.1.16"
-            // ,port: 8545
-        }
-        , "staging": {
+      // unused networks
+        // "live": {
+        //     network_id: 1 // Ethereum public network
+        //     // ,host: "192.168.1.16"
+        //     // ,port: 8545
+        // }
+        // , "morden": {
+        //     network_id: 2       // Official Ethereum test network
+        //     // ,host: "192.168.1.16"
+        //     // ,port: 8545
+        // }
+         "staging": {
             network_id: 14658 // b9labs student network
             , host: "192.168.1.16"
             , port: 8545
